411257d6af707c9accb921d6a2f0538164573496
Bruno Nova <brunomb.nova@gmail.com>
2015-04-22 17:48:29 +0100

    Add initial methods

--- extension/nautilus-hide.py
+++ extension/nautilus-hide.py
@@ -2,11 +2,22 @@ from gi.repository import Nautilus, GObject
 from gettext import gettext, locale, bindtextdomain, textdomain
 
 class NautilusHide(Nautilus.MenuProvider, GObject.GObject):
+	"""Simple Nautilus extension that adds some actions to the context menu to
+	hide and unhide files, by adding/removing their names to/from the folder's
+	'.hidden' file."""
 	def __init__(self):
 		pass
 
 	def get_file_items(self, window, files):
-		return
+		"""Returns the menu items to display when one or more files/folders are
+		selected."""
+		# Add the menu items
+		items = []
+		self._setup_gettext();
+		items += [self._create_hide_item(files)]
+		items += [self._create_unhide_item(files)]
+
+		return items
 
 
 	def _setup_gettext(self):
@@ -15,5 +26,22 @@ class NautilusHide(Nautilus.MenuProvider, GObject.GObject):
 			locale.setlocale(locale.LC_ALL, "")
 		except:
 			pass
-		bindtextdomain("nautilus-admin", "@CMAKE_INSTALL_PREFIX@/share/locale")
-		textdomain("nautilus-admin")
+		bindtextdomain("nautilus-hide", "@CMAKE_INSTALL_PREFIX@/share/locale")
+		textdomain("nautilus-hide")
+
+	def _create_hide_item(self, files):
+		"""Creates the 'Hide file(s)' menu item."""
+		pass
+
+	def _create_unhide_item(self, files):
+		"""Creates the 'Unhide file(s)' menu item."""
+		pass
+
+
+	def _hide_run(self, menu, files):
+		"""'Hide file(s)' menu item callback."""
+		pass
+
+	def _unhide_run(self, menu, files):
+		"""'Unhide file(s)' menu item callback."""
+		pass
