2af732a2c5dd22cbfcf163ed3c2bfc8975eab391
Bruno Nova <4999472+brunonova@users.noreply.github.com>
2015-04-29 15:24:21 +0100

    Refactoring, improvements and corrections

--- extension/nautilus-hide.py
+++ extension/nautilus-hide.py
@@ -13,12 +13,14 @@ class NautilusHide(Nautilus.MenuProvider, GObject.GObject):
 		"""Returns the menu items to display when one or more files/folders are
 		selected."""
 		# Make "files" paths relative and remove files that start with '.'
-		# or that end with '~'
-		dir_path = None
+		# or that end with '~' (files that are already hidden)
+		dir_path = None # path of the directory
 		filenames = []
 		for file in files:
-			if dir_path == None:
+			if dir_path == None: # first file: find path to directory
 				dir_path = file.get_parent_location().get_path()
+				if file.get_uri_scheme() != "file": # must be a local directory
+					return
 			name = file.get_name()
 			if not name.startswith(".") and not name.endswith("~"):
 				filenames += [name]
@@ -26,37 +28,40 @@ class NautilusHide(Nautilus.MenuProvider, GObject.GObject):
 		if dir_path == None or len(filenames) == 0:
 			return
 
-		# Check if the user can edit the ".hidden" file
-		if os.path.exists(dir_path + "/.hidden"):
-			if not os.access(dir_path + "/.hidden", os.R_OK | os.W_OK):
-				return
-		else:
-			if not os.access(dir_path, os.W_OK | os.X_OK):
-				return
+		# Check if the user has write access to the ".hidden" file and its
+		# directory
+		hidden_path = dir_path + "/.hidden" # path to the ".hidden" file
+		if not os.access(dir_path, os.W_OK | os.X_OK) or \
+		   (os.path.exists(hidden_path) and not os.access(hidden_path, os.R_OK | os.W_OK)):
+			return
 
 		# Read the ".hidden" file
-		if os.path.exists(dir_path + "/.hidden"):
-			with open(dir_path + "/.hidden") as f:
-				hidden = {line.strip() for line in f.readlines()}
-		else:
+		try:
+			if os.path.exists(hidden_path):
+				with open(hidden_path) as f:
+					hidden = {line.strip() for line in f.readlines()}
+			else:
+				hidden = set()
+		except: # ".hidden" file was deleted?
 			hidden = set()
 
-		# Count number of files that can be hidden and unhidden
-		# TODO Only count total no. of files, and check if hidden and visible files exist
-		nhidden, nvisible = 0, 0
+		# Determine what menu items to show (Hide, Unhide, or both)
+		show_hide, show_unhide = False, False
 		for file in filenames:
 			if file in hidden:
-				nhidden += 1
+				show_unhide = True
 			else:
-				nvisible += 1
+				show_hide = True
+			if show_hide and show_unhide:
+				break
 
 		# Add the menu items
 		items = []
 		self._setup_gettext();
-		if nvisible > 0:
-			items += [self._create_hide_item(filenames, dir_path, hidden, nvisible)]
-		if nhidden > 0:
-			items += [self._create_unhide_item(filenames, dir_path, hidden, nhidden)]
+		if show_hide:
+			items += [self._create_hide_item(filenames, hidden_path, hidden)]
+		if show_unhide:
+			items += [self._create_unhide_item(filenames, hidden_path, hidden)]
 
 		return items
 
@@ -70,9 +75,9 @@ class NautilusHide(Nautilus.MenuProvider, GObject.GObject):
 		bindtextdomain("nautilus-hide", "@CMAKE_INSTALL_PREFIX@/share/locale")
 		textdomain("nautilus-hide")
 
-	def _create_hide_item(self, files, dir_path, hidden, nvisible):
+	def _create_hide_item(self, files, hidden_path, hidden):
 		"""Creates the 'Hide file(s)' menu item."""
-		if nvisible == 1:
+		if len(files) == 1:
 			item = Nautilus.MenuItem(name="NautilusHide::HideFile",
 				                     label=gettext("Hide File"),
 				                     tip=gettext("Hide this file"))
@@ -80,12 +85,12 @@ class NautilusHide(Nautilus.MenuProvider, GObject.GObject):
 			item = Nautilus.MenuItem(name="NautilusHide::HideFiles",
 				                     label=gettext("Hide Files"),
 				                     tip=gettext("Hide these files"))
-		item.connect("activate", self._hide_run, files, dir_path, hidden)
+		item.connect("activate", self._hide_run, files, hidden_path, hidden)
 		return item
 
-	def _create_unhide_item(self, files, dir_path, hidden, nhidden):
+	def _create_unhide_item(self, files, hidden_path, hidden):
 		"""Creates the 'Unhide file(s)' menu item."""
-		if nhidden == 1:
+		if len(files) == 1:
 			item = Nautilus.MenuItem(name="NautilusHide::UnhideFile",
 				                     label=gettext("Unhide File"),
 				                     tip=gettext("Unhide this file"))
@@ -93,29 +98,36 @@ class NautilusHide(Nautilus.MenuProvider, GObject.GObject):
 			item = Nautilus.MenuItem(name="NautilusHide::UnhideFiles",
 				                     label=gettext("Unhide Files"),
 				                     tip=gettext("Unhide these files"))
-		item.connect("activate", self._unhide_run, files, dir_path, hidden)
+		item.connect("activate", self._unhide_run, files, hidden_path, hidden)
 		return item
 
+	def _update_hidden_file(self, hidden_path, hidden):
+		"""Updates the '.hidden' file with the new filenames, or deletes it if
+		empty (no files to hide)."""
+		try:
+			if hidden == set():
+				if os.path.exists(hidden_path):
+					os.remove(hidden_path)
+			else:
+				with open(hidden_path, "w") as f:
+					for file in hidden:
+						f.write(file + '\n')
+			# TODO Refresh Nautilus?
+		except:
+			print("Failed to delete or write to {}!".format(hidden_path))
 
-	def _hide_run(self, menu, files, dir_path, hidden):
+
+	def _hide_run(self, menu, files, hidden_path, hidden):
 		"""'Hide file(s)' menu item callback."""
 		for file in files:
 			hidden.add(file)
-		with open(dir_path + "/.hidden", "w") as f:
-			for file in hidden:
-				f.write(file + '\n')
-		# TODO Remove file if empty
-		# TODO Refresh Nautilus?
+		self._update_hidden_file(hidden_path, hidden)
 
-	def _unhide_run(self, menu, files, dir_path, hidden):
+	def _unhide_run(self, menu, files, hidden_path, hidden):
 		"""'Unhide file(s)' menu item callback."""
 		for file in files:
 			try:
 				hidden.remove(file)
-			except:
+			except: # file not in "hidden"
 				pass
-		with open(dir_path + "/.hidden", "w") as f:
-			for file in hidden:
-				f.write(file + '\n')
-		# TODO Remove file if empty
-		# TODO Refresh Nautilus?
+		self._update_hidden_file(hidden_path, hidden)
